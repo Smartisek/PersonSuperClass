0c01332e775a621325a590d4c60ebabf441b3ed6
Smartisek <domalipdominik@gmail.com>
2023-10-19 17:18:06 +0100

    àdding instructor, create one each and print

--- src/Instructor.java
+++ src/Instructor.java
@@ -1,6 +1,19 @@
 
 
-public class Instructor {
+public class Instructor extends Person{
+
+    private float salary;
 
 
+    public Instructor(String name, int date_of_birth, float salary){
+        super(name, date_of_birth);
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "salary=" + salary +
+                "} " + super.toString();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
 
         Person person1 = new Person("Dominik", 1999);
+        System.out.println(person1);
+
+        Student student1 = new Student("Jack", 2001, "Engineering");
+        System.out.println(student1);
+
+        Instructor instructor1 = new Instructor("Dermont", 1985, 60000.00f);
+        System.out.println(instructor1);
+    }
+
 
 }
